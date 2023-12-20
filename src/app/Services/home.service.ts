8a27723f54f2822fa5a4eda5fb5d817619039c8a
m.ibrahim.ext <m.ibrahim.ext@TAH32VAXJN>
2023-12-20 14:24:57 +0200

    ...

--- src/app/Services/home.service.ts
+++ src/app/Services/home.service.ts
@@ -8,45 +8,68 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class HomeService {
-  message :string = 'This message from Home Service !!'; 
-  numberOfActiveCourse = new BehaviorSubject(0); //
-  courses:any=[{}];
-  constructor(private http :HttpClient,private toastr:ToastrService, private spinner:NgxSpinnerService) { }
-  user:string ='dana@gmail.com'; 
-  GetAllCourses(){
-    this.http.get('https://localhost:7234/api/Course/GetAllCourse').subscribe((resp)=>{
-      this.courses = resp;
-      localStorage.setItem('username', this.user);
-      localStorage.getItem('username');
-    },err=>{ 
-      console.log(err.message);
-      console.log(err.status);
-      
-     
-    })
+  message: string = 'This message from Home Service !!'; 
+  numberOfActiveCourse = new BehaviorSubject<number>(0); // Specify the type for BehaviorSubject
+  courses: any = [{}];
+  user: string = 'dana@gmail.com'; 
+  
+  constructor(
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) { }
+
+  GetAllCourses() {
+    this.http.get('https://localhost:7234/api/Course/GetAllCourse').subscribe(
+      (resp) => {
+        this.courses = resp;
+        localStorage.setItem('username', this.user);
+        localStorage.getItem('username');
+      },
+      (err) => { 
+        console.log(err.message);
+        console.log(err.status);
+      }
+    );
   }
 
-  DeleteCourse(id:number){
-      debugger;
-      this.spinner.show;
-    this.http.delete('https://localhost:7234/api/Course/DeleteCourse/'+id).subscribe((resp)=>{
-      this.toastr.success('Deleted')
-    },err=>{
-    this.toastr.error
-    this.spinner.hide;
-
-    })
+  DeleteCourse(id: number) {
+    this.spinner.show();
+    this.http.delete(`https://localhost:7234/api/Course/DeleteCourse/${id}`).subscribe(
+      (resp) => {
+        this.toastr.success('Deleted');
+      },
+      (err) => {
+        this.toastr.error('Error occurred');
+      }
+    );
+    this.spinner.hide();
   }
 
-  CreateCourse(body:any){
-    debugger
-    this.http.post('https://localhost:7234/api/Course/CreateCourse',body).subscribe((resp) =>{
-      alert('Created')
+  CreateCourse(body: any) {
+    this.http.post('https://localhost:7234/api/Course/CreateCourse', body).subscribe(
+      (resp) => {
+        this.toastr.success('Created');
+        // Avoid window reload here if it disrupts user experience
+      },
+      (err) => {
+        this.toastr.error('Something went wrong');
+      }
+    );
+  }
 
-    },
-    err=>
-    alert('something want wrong'))
-  window.location.reload();
-
+  UpdateCourse(body: any) {
+    this.spinner.show();
+    this.http.put('https://localhost:7234/api/Course/', body).subscribe(
+      (resp) => {
+        this.courses = resp;
+        this.toastr.success('Updated');
+        this.spinner.hide();
+      },
+      (err) => {
+        this.toastr.error('Error occurred');
+        this.spinner.hide();
+      }
+    );
   }
 }
--- src/app/admin/manage-course/manage-course.component.ts
+++ src/app/admin/manage-course/manage-course.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { HomeService } from 'src/app/Services/home.service';
 import { CreateCourseComponent } from '../create-course/create-course.component';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms'; // Import form-related modules
 
 @Component({
   selector: 'app-manage-course',
@@ -10,29 +10,50 @@ import { CreateCourseComponent } from '../create-course/create-course.component'
   styleUrls: ['./manage-course.component.css']
 })
 export class ManageCourseComponent implements OnInit {
-  @ViewChild  ('callDeleteDailog')callDelete!:TemplateRef<any>
-  constructor(public home:HomeService,public dialog: MatDialog){}
+  @ViewChild('callDeleteDailog') callDelete!: TemplateRef<any>;
+  @ViewChild('callUpdateDialog') callUpdateDialog!: TemplateRef<any>;
+  updateForm:FormGroup = new FormGroup({
+    courseid :new FormControl(),
+     coursename:new FormControl('',Validators.required),
+     price:new FormControl('',Validators.required),
+     startDate:new FormControl('',Validators.required),
+     endDate:new FormControl('',Validators.required),
+  });
+
+  constructor(public home: HomeService, public dialog: MatDialog) { }
+
   ngOnInit(): void {
-   this.home.GetAllCourses();
+    this.fetchCourses();
   }
 
-  DeleteCourse(id:number){
-    const dialogRef= this.dialog.open(this.callDelete);
-    dialogRef.afterClosed().subscribe((result)=>{
-      if(result=='1'){
-        debugger;
+  fetchCourses() {
+    this.home.GetAllCourses();
+  }
+
+  DeleteCourse(id: number) {
+    const dialogRef = this.dialog.open(this.callDelete);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === '1') {
         this.home.DeleteCourse(id);
+      } else {
+        console.log('Thank you');
       }
-  
-    else {
-      console.log('Thank you ');
-    }
-    })
+    });
+  }
 
-    
+  OpenCreateDialog() {
+    this.dialog.open(CreateCourseComponent);
   }
-  OpenCreateDialog(){
-      this.dialog.open(CreateCourseComponent)
+
+  pData:any ;
+  openUpdateDailog(obj:any){
+    this.pData = obj;
+    console.log(this.pData);
+    this.updateForm.controls['courseid'].setValue(this.pData.courseid);
+    this.dialog.open(this.callUpdateDialog);
   }
- 
+  UpdateCourse(){
+    debugger;
+    this.home.UpdateCourse(this.updateForm.value);
+  }
 }
